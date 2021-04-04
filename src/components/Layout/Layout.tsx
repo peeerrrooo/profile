0b3c7d03e0ec78a82c889d8e1f3c5f44615beb9f
Daniil Brakk <peeerrrooo@gmail.com>
2021-04-04 21:37:51 +0300

    MAIN: Remove particles

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,7 +2,6 @@ import React, { ReactNode } from 'react'
 import { BackgroundLines } from 'components/BackgroundLines'
 import styled from 'styled-components'
 import { Menu } from 'components/Menu'
-import UiParticles, { IParticlesParams } from 'react-particles-js'
 import { IMenuModel } from 'models/menuModel/types'
 import { inject, observer } from 'mobx-react'
 import { IModels } from 'models/models/types'
@@ -14,47 +13,12 @@ export interface ILayoutProps {
   MenuModel?: IMenuModel
 }
 
-const particleConfig: IParticlesParams = {
-  particles: {
-    number: {
-      value: 160,
-      density: {
-        enable: false
-      }
-    },
-    color: {
-      value: '#ffffff'
-    },
-    opacity: {
-      value: 0.1
-    },
-    size: {
-      value: 5,
-      random: true,
-      anim: {
-        speed: 4,
-        size_min: 0.3
-      }
-    },
-    line_linked: {
-      enable: false
-    },
-    move: {
-      random: true,
-      speed: 1,
-      direction: 'top',
-      out_mode: 'out'
-    }
-  }
-}
-
 const Layout = inject((store: IModels) => ({
   MenuModel: store?.MenuModel
 }))(
   observer(({ children, MenuModel }: ILayoutProps) => {
     return (
       <Root$ element="main" mediaCommon={{ touch: { paddingBottom: 's6' } }}>
-        <Particles$ params={particleConfig} />
         <Menu />
         <BackgroundLines />
         <Content$ element="section" open={MenuModel!.opened}>
@@ -91,14 +55,6 @@ const Content$ = styled(Block)<{ open: boolean }>`
   }
 `
 
-const Particles$ = styled(UiParticles)`
-  position: absolute;
-  left: 0;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  z-index: -1;
-`
 //#endregion
 
 export { Layout }
