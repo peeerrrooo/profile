60ebbc600d0420788c8b97bc686561123b8980a0
Daniil Brakk <peeerrrooo@gmail.com>
2021-09-23 00:58:18 +0300

    [#AVATAR]: Update photo

--- src/utils/info.ts
+++ src/utils/info.ts
@@ -1,5 +1,5 @@
 import { getAge } from './moment'
-import aboutImage from 'resources/img/about-image.jpg'
+import aboutImage from 'resources/img/about-image.jpeg'
 import ujet from 'resources/img/ujet.svg'
 import ismart from 'resources/img/ismart.svg'
 import finservice from 'resources/img/finservice.svg'
