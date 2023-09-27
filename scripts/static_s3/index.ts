268bcdb136322c3f3511fcbb82a7e2e325b1dce0
Daniil Brakk <peeerrrooo@gmail.com>
2023-09-27 21:51:22 +0300

    fix(ci/cd): fix s3 step

--- scripts/static_s3/index.ts
+++ scripts/static_s3/index.ts
@@ -1,5 +1,5 @@
 import { S3Client } from '@aws-sdk/client-s3'
-import { deleteStatic, uploadFileToBucket } from './s3'
+import { uploadFileToBucket } from './s3'
 import { getAllFiles } from './files'
 
 const s3Client = new S3Client({
@@ -9,8 +9,6 @@ const s3Client = new S3Client({
 const bucketName = process.env.UPLOAD_BUCKET_NAME || ''
 
 const bootstrap = async () => {
-  await deleteStatic(s3Client, bucketName)
-
   const files = getAllFiles('./build/static')
   const req$: Array<Promise<void>> = []
   for (const file of files) {
