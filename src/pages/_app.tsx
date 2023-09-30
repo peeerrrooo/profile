cb4d4d00bd5b3da51b39c27b1acc630825ccb1d1
Daniil Brakk <peeerrrooo@gmail.com>
2023-09-30 11:06:19 +0300

    feat: update yandex metrics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -88,29 +88,25 @@ class App extends NextApp<AppProps> {
                 type="text/javascript"
                 dangerouslySetInnerHTML={{
                   __html: `
-                  (function(m,e,t,r,i,k,a){m[i] = m[i] || function() {(m[i].a = m[i].a || []).push(arguments)};
-                    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-                    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+                  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-                    ym(75140080, "init", {
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true,
-                    webvisor:true
-                  })
+   ym(95106904, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
                 `
                 }}
               />
               <noscript
                 dangerouslySetInnerHTML={{
                   __html: `
-                  <div>
-                    <img
-                      src="https://mc.yandex.ru/watch/75140080"
-                      style="position:absolute; left:-9999px;"
-                      alt=""
-                    />
-                  </div>
+                  <div><img src="https://mc.yandex.ru/watch/95106904" style="position:absolute; left:-9999px;" alt="" /></div>
               `
                 }}
               />
